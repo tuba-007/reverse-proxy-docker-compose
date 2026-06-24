355a05724ceed058b2756062b560c2174f272166
tuba <tuba.k9889@gmail.com>
2026-06-24 23:58:17 +0530

    pushing an minimal server code

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 const { Client } = require('pg')
+const express = require('express')
 
+const app = express()
 const client = new Client({
   connectionString: process.env.DATABASE_URL
 })
@@ -7,3 +9,9 @@ const client = new Client({
 client.connect()
   .then(() => console.log('connected to database'))
   .catch(err => console.error('connection error', err))
+
+app.get('/', (req, res) => {
+  res.send('app is running and connected to database')
+})
+
+app.listen(3000, () => console.log('App running on port 3000'))
